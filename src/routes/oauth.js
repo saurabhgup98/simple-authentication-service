bcda328ca6eca631ac962b754d380e73aed78b62
saurabhgup98 <saurabhgup98@gmail.com>
2025-09-25 21:52:34 +0530

    Fix OAuth routes: add missing Facebook and GitHub OAuth routes
    - Updated oauth.js routes to include all three OAuth providers
    - Added Facebook OAuth routes: /facebook, /facebook/callback, /facebook/url
    - Added GitHub OAuth routes: /github, /github/callback, /github/url
    - Fixed internal server errors caused by missing route imports

--- src/routes/oauth.js
+++ src/routes/oauth.js
@@ -1,5 +1,9 @@
 import express from 'express';
-import { googleLogin, googleCallback, getGoogleAuthUrl } from '../controllers/oauthController.js';
+import { 
+  googleLogin, googleCallback, getGoogleAuthUrl,
+  facebookLogin, facebookCallback, getFacebookAuthUrl,
+  githubLogin, githubCallback, getGitHubAuthUrl
+} from '../controllers/oauthController.js';
 
 const router = express.Router();
 
@@ -7,5 +11,15 @@ const router = express.Router();
 router.get('/google', googleLogin);
 router.get('/google/callback', googleCallback);
 router.get('/google/url', getGoogleAuthUrl);
+
+// Facebook OAuth routes
+router.get('/facebook', facebookLogin);
+router.get('/facebook/callback', facebookCallback);
+router.get('/facebook/url', getFacebookAuthUrl);
+
+// GitHub OAuth routes
+router.get('/github', githubLogin);
+router.get('/github/callback', githubCallback);
+router.get('/github/url', getGitHubAuthUrl);
 
 export default router;
